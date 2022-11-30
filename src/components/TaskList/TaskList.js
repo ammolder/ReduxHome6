99cca058507342d5d3ab49dfa1c96182eb7018cf
ammolder <98980721+ammolder@users.noreply.github.com>
2022-11-30 02:03:31 +0100

    added change

---
+++ src/components/TaskList/TaskList.js
@@ -0,0 +1,38 @@
+// Імпортуємо хук
+import { useSelector } from 'react-redux';
+import { Task } from 'components/Task/Task';
+// Імпортуємо об'єкт значень фільтра
+import { statusFilters } from 'redux/constants';
+import css from './TaskList.module.css';
+
+const getVisibleTasks = (tasks, statusFilter) => {
+  console.log('tasks :', tasks);
+  console.log('statusFilter :', statusFilter);
+  switch (statusFilter) {
+    case statusFilters.active:
+      return tasks.filter(task => !task.completed);
+    case statusFilters.completed:
+      return tasks.filter(task => task.completed);
+    default:
+      return tasks;
+  }
+};
+
+export const TaskList = () => {
+  // Отримуємо масив завдань із стану Redux
+  const tasks = useSelector(state => state.tasks);
+  // Отримуємо значення фільтра із стану Redux
+  const statusFilter = useSelector(state => state.filters.status);
+  // Обчислюємо масив завдань, які необхідно відображати в інтерфейсі
+  const visibleTasks = getVisibleTasks(tasks, statusFilter);
+
+  return (
+    <ul className={css.list}>
+      {visibleTasks.map(task => (
+        <li className={css.listItem} key={task.id}>
+          <Task task={task} />
+        </li>
+      ))}
+    </ul>
+  );
+};
